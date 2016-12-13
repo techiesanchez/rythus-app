9fd486a68ef26772ae7167de3c5aa1a8ce0207ae
Chris Grimmett <chris@grimtech.net>
2016-12-12 22:28:34 -0800

    progress

--- gulpfile.js
+++ gulpfile.js
@@ -5,38 +5,78 @@ var uglify = require('gulp-uglify');
 var streamify = require('gulp-streamify');
 var rename = require('gulp-rename');
 var browserSync = require('browser-sync').create();
-
-
+var jsoncombine = require('gulp-jsoncombine');
+var jsonlint = require('gulp-jsonlint');
+var cardBundler = require('./lib/card-bundler');
+var csslint = require('gulp-csslint');
+var image = require('gulp-image');
 
 
 
 gulp.task('browserify', function() {
-    var bundleStream = browserify('./lib/cards_rewards.js')
+    browserify('./lib/cards_rewards.js')
 	.bundle()
-
-    bundleStream
 	.pipe(source('cards_rewards.js'))
 	.pipe(streamify(uglify()))
 	.pipe(rename('bundle.js'))
-	.pipe(gulp.dest('./'))
+	.pipe(gulp.dest('./dist'))
+	.pipe(browserSync.stream())
+});
 
+
+gulp.task('lint.cards', function() {
+    gulp.src('./cards/**/*.json')
+	.pipe(jsonlint())
+        .pipe(jsonlint.reporter());
+});
+
+gulp.task('build.cards', function() {
+    gulp.src('./cards/**/*.json')
+        .pipe(jsoncombine("cards.json", cardBundler))
+	.pipe(gulp.dest('./dist'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('build.images', function() {
+    gulp.src('./contrib/**/*.{png,jpg,jpeg,svg}')
+	.pipe(image())
+	.pipe(gulp.dest('./dist/img/'));
+});
+
+gulp.task('watch.images', function() {
+    gulp.watch('./contrib/**/*.{png|jpg|jpeg|gif}', ['build.images']);
+});
+
+gulp.task('watch.cards', function() {
+    gulp.watch('cards/**/*.json', ['build.cards']);
 });
 
 
 gulp.task('watch', function() {
     gulp.watch('lib/**/*.js', ['browserify']);
+    gulp.watch('./*.css', ['styles.build']);
 });
 
 
 gulp.task('browser-sync', function() {
     browserSync.init({
 	server: {
-	    baseDir: './'
+	    baseDir: './dist'
 	}
     });
 });
 
+gulp.task('styles.build', function() {
+    gulp.src('./*.css')
+	.pipe(csslint({
+	    'ids': false
+	}))
+	.pipe(csslint.formatter())
+	.pipe(gulp.dest('./dist'))
+	.pipe(browserSync.stream())
+});
 
 
-gulp.task('default', ['browserify', 'watch', 'browser-sync']);
-gulp.task('build', ['browserify']);
+
+gulp.task('default', ['browserify', 'watch', 'watch.images', 'watch.cards', 'browser-sync']);
+gulp.task('build', ['browserify', 'styles.build', 'lint.cards', 'build.cards', 'build.images']);
